9f86fdec86f7a5a3472cd237147eb9b2888cec7a
Nikita Zhuravlev <skill1993@icloud.com>
2021-05-23 11:59:32 +0300

    Made interface and logic

--- RGBSliderHW/ViewController.swift
+++ RGBSliderHW/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewOfColors: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueSlider: UILabel!
+    
+    var viewColor = UIColor()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        defineColor()
     }
-
-
+    
+    @IBAction func redSliderValueChanged() {
+        redValueLabel.text = String(redSlider.value)
+        defineColor()
+    }
+    
+    @IBAction func greenSliderValueChanged() {
+        greenValueLabel.text = String(greenSlider.value)
+        defineColor()
+    }
+    
+    @IBAction func blueSliderValueChanged() {
+        blueValueSlider.text = String(blueSlider.value)
+        defineColor()
+    }
+    
+    
+    
+    func defineColor() {
+        viewColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0) 
+        viewOfColors.backgroundColor = viewColor
+    }
+    
+    
 }
 
